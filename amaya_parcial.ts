52b26407c64327ce9c4d637cdbf4bbc1b04f4293
AirinAmaya <amariuthamaya@uniguajira.edu.co>
2021-10-20 09:57:57 -0500

    parcial2

---
+++ amaya_parcial.ts
@@ -0,0 +1,55 @@
+"use strict" ;
+deje tabla= mantenimiento;
+let id = i++ ;
+let date = "fecha";
+let scrip = "descripcion";
+let true = "resultados" ;
+deje tabla = equipo;
+let id = 1;
+let marca = tipo;
+let modelo = año;
+console . log ( "mantenimiento");
+console . log ('el id es  $ { i++ } ');
+console . log ('la fecha es $ { date} ');
+console . log ('la descripsion del producto  es $ { scrip } ');
+console . log ('el resultado es $ { true} ');
+console . log ('el tipo de marca es $ {marca}');
+console . log ('el año del automovil es $ { modelo }')
+let lista = ["scrip", "true", "marca", "modelo" ] ;
+consola . log (lista; )
+for ( t index = 0; index < lista . length ; index ++ ) {consola log (lista [indice] );
+}
+lista . forEach(element => {
+    consola . log ( elemento) ;
+});
+let tuple = [1 , 20/10/2000, daño de llantas, falso, toyota, 2020 ];
+console . log (tuple  [ 1 ] );
+let tuple2 =  [1, 18/10/2000, daño de motor, verdadero, honda, 2011];
+consola . log (tuple2  [ 1 ]);
+tuple2.forEach(element)  => {
+    console . log(element);
+});
+let mivariable = "Automotriz ";
+console.log(Automotriz);
+Automotriz = mantenimiento
+console.log(Automotriz);
+const arreglo1 = {
+    id: 1,
+    fecha: 20/10/2020,
+    descripcion: "ingreso de auto por cambio de inyectores",
+    resultado : "verdadero"),
+    tipo : "mazda",
+    modelo : 2021
+    }
+    
+    console . log (arreglo1.descripcion + arreglo1.resultado);
+
+const arreglo2 = {
+    id: 2,
+    fecha: 20/10/2011,
+    descripcion: "reparacion de motor",
+    resultado : "falso"),
+    tipo : "toyota",
+    modelo : 2006
+}
+console . log (arreglo2.descripcion + arreglo2.resultado);
